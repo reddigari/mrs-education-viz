59f691afabcf8242da846a95e72d9a84c69ac91c
Samir Reddigari <samir@brainspecmed.com>
2024-10-19 14:31:18 -0400

    feat: add first-order phase slider

--- phasing.js
+++ phasing.js
@@ -1,11 +1,3 @@
-//const width = 640;
-//const height = 400;
-//const marginTop = 20;
-//const marginRight = 20;
-//const marginBottom = 30;
-//const marginLeft = 40;
-
-
 function Config(width=640) {
     this.width = width;
     this.height = 0.8 * width;
@@ -59,13 +51,22 @@ function plotData(data, cfg, updateScale=false) {
       .attr("d", line(data));
 }
 
-function adjustPhase(data, phase) {
-    const phaseScale = d3.scaleLinear([0, 100], [0, Math.PI]);
-    const phaseRad = phaseScale(phase);
-    document.querySelector("#phaseValue").innerHTML = `${phaseRad.toFixed(2)} rad`;
-    const phasedData = data.map(i => rotate(i, phaseRad));
+
+function degToRad(deg) { return deg * math.pi / 180 };
+function radToDeg(rad) { return rad * 180 / math.pi };
+
+function phaseData(data, phases) {
+    const phase0Scale = d3.scaleLinear([0, 100], [0, Math.PI]);
+    const phase1Scale = d3.scaleLinear([0, 100], [0, Math.PI]);
+    const phasesRad = {"ph0": phase0Scale(phases.ph0), "ph1": phase1Scale(phases.ph1)};
+    console.log(phasesRad);
+    document.querySelector("#phase0Value").innerHTML = `${phasesRad.ph0.toFixed(2)} rad`;
+    document.querySelector("#phase1Value").innerHTML = `${phasesRad.ph1.toFixed(2)} rad/ppm`;
+    const phasedData0 = data.map(d => rotate(d, phasesRad.ph0));
+    const ph1Angles = data.map(d => phasesRad.ph1 * d.ppm);
+    const out = phasedData0.map((d, i) => rotate(d, ph1Angles[i]));
     //plotData(phasedData, cfg, false);
-    return phasedData
+    return out;
 }
 
 function rotate(item, phase) {
@@ -74,6 +75,21 @@ function rotate(item, phase) {
     const angle = math.exp(phaseI);
     const out = math.multiply(cn, angle);
     return {"ppm": item.ppm, "real": out.re, "imag": out.im};
+}
+
+
+function getPhaseInputs() {
+    const p0 = document.querySelector("#phase0");
+    const p1 = document.querySelector("#phase1");
+    return {"ph0": p0.value, "ph1": p1.value};
+}
+
+
+function update(data, cfgReal, cfgImag) {
+    const phases = getPhaseInputs();
+    const phasedData = phaseData(data, phases);
+    plotData(phasedData, cfgReal, false);
+    plotData(phasedData, cfgImag, false);
 }
 
 async function onReady() {
@@ -85,13 +101,11 @@ async function onReady() {
     cfgImag.chartId = "chart-imag";
     plotData(data, cfgReal, true);
     plotData(data, cfgImag, true);
-    const slider = document.querySelector("#phase0");
-    slider.addEventListener("input", function(evt) {
-        const phasedData = adjustPhase(data, evt.target.value)
-        plotData(phasedData, cfgReal, false);
-        plotData(phasedData, cfgImag, false);
+    document.querySelectorAll(".slider").forEach(slider => {
+        slider.addEventListener("input", function(evt) {
+            update(data, cfgReal, cfgImag);
+        });
     });
-
 }
 
 document.addEventListener("DOMContentLoaded", () =>
